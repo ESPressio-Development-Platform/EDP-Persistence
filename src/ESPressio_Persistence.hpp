6cb05e5ab1d1d2c6f15a57c263da7ca252b08e95
Simon Stuart <lakraven@gmail.com>
2026-09-22 05:37:01 +0200

    Expose complete abstract Persistence contract; refs #1

--- src/ESPressio_Persistence.hpp
+++ src/ESPressio_Persistence.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "persistence/FileStorageContract.hpp"
+#include "persistence/KeyValueStorageContract.hpp"
 #include "persistence/PersistenceComposition.hpp"
+#include "persistence/PersistenceProviderValidation.hpp"
 #include "persistence/PersistenceTypes.hpp"
 #include "persistence/PersistenceViews.hpp"
